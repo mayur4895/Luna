ddda86ebd4800bf5b23613ddeb26e47370beb640
mayur shinde <kssmsslss330@gmail.com>
2024-09-03 22:07:55 +0530

    "done last"

--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -26,14 +26,14 @@ export function  TabsComponent() {
       </TabsList>
       <TabsContent value="aboutme">
       <div className="  text-[#969696] font-sans h-full w-full   "> 
- Hello! I'm Dave, your sales rep here from Salesforce. I've been working at this awesome company for 3 years now.
+ Hello! I am Dave, your sales rep here from Salesforce. I have been working at this awesome company for 3 years now.
    I was born and raised in Albany, NY& have been living in Santa Carla for the past 10 years my wife Tiffany and my 4 year old twin daughters- Emma and Ella. Both of them are just starting school, so my calender is usually blocked between 9-10 AM. This is a... 
 </div>
       </TabsContent>
       <TabsContent value="experiences">
       <p className="  text-[#969696] font-sans ">
  
- Hello! I'm Dave, your sales rep here from Salesforce. I've been working at this awesome company for 3 years now.
+ Hello! Im Dave, your sales rep here from Salesforce. Ive been working at this awesome company for 3 years now.
  
  <br /> <br /> ed in Santa Carla for the past 10 years my wife Tiffany and my 4 year old twin daughters- Emma and Ella. Both of them are just starting school, so my calender is usually blocked between 9-10 AM. This is a... 
 </p>
@@ -42,7 +42,7 @@ export function  TabsComponent() {
       <TabsContent value="recommonded">
       <p className="  text-[#969696] font-sans ">
  
- Hello! I'm Dave, your sales rep here from Salesforce. I've been working at this awesome company for 3 years now.
+ Hello! Im Dave, your sales rep here from Salesforce. Ive been working at this awesome company for 3 years now.
  
  <br /> <br />
   I was born and raised in Alban years my wife Tiffany and my 4 year old twin daughters- Emma and Ella. Both of them are just starting school, so my calender is usually blocked between 9-10 AM. This is a... 
